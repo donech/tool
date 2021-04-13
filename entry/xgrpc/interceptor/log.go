eebed167cd2c9dd063f077c31bc65be3074db7e8
朴伟雄 <2355468258@qq.com>
2021-04-13 18:01:51 +0800

    完善 grpc LogInterceptor

--- entry/xgrpc/interceptor/log.go
+++ entry/xgrpc/interceptor/log.go
@@ -3,6 +3,8 @@ package interceptor
 import (
 	"context"
 
+	"github.com/pkg/errors"
+
 	"go.uber.org/zap"
 
 	"github.com/donech/tool/xlog"
@@ -15,6 +17,6 @@ type LogInterceptor struct{}
 func (i *LogInterceptor) Serve(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	xlog.L(ctx).Info("incoming grpc req", zap.Reflect("req", req))
 	resp, err = handler(ctx, req)
-	xlog.L(ctx).Info("output grpc resp", zap.Reflect("resp", resp))
+	xlog.L(ctx).Info("output grpc resp", zap.Reflect("resp", resp), zap.Error(errors.WithStack(err)))
 	return resp, err
 }
